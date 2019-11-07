b0acef26822a27f5c97806dfe20a5fb865d8fce3
Talosdx <talosdx@gmail.com>
2019-11-08 02:58:58 +0300

    javadoc [StringUtils] add docs for some methods;

--- src/main/java/dev/talosdx/util/StringUtils.java
+++ src/main/java/dev/talosdx/util/StringUtils.java
@@ -25,11 +25,21 @@ public class StringUtils
         return !isEmpty(string);
     }
     
+    /**
+     * Check string on empty include spaces
+     * @param string the string we check
+     * @return true if string has only spaces or empty or null
+     */
     public static boolean isEmpty(String string)
     {
         return string == null || string.isEmpty() || removeSpaces(string).isEmpty();
     }
     
+    /**
+     * use check {@link #isEmpty(String)}, if string is empty throw exception else return string
+     * @param string the string we check
+     * @return string unchanged
+     */
     public static String requiredNotEmpty(String string)
     {
         if (isEmpty(string))
@@ -39,6 +49,11 @@ public class StringUtils
         return string;
     }
     
+    /**
+     * Remove any spaces include non-breaking space
+     * @param string the string we clear
+     * @return string without spaces
+     */
     public static String removeSpaces(String string)
     {
         return string.replaceAll("[\\s\\u00A0]", "");
